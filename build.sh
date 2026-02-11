8ccbd76fc6e1ce155313d384d7dfd13e4af2fe7c
Thierry Berger <contact@thierryberger.com>
2026-02-11 11:57:34 +0100

    fix build, use workaround from https://www.getzola.org/documentation/deployment/cloudflare-pages/#or-use-unstable-pre-build-environment-variable-asdf

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-ZOLA_VERSION=0.22.1
-
-curl -sLJO "https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
-tar -xf "zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
-
-./zola build
