d45456ea27e23e62b3a6f8a6763e61c015b53532
Thierry Berger <contact@thierryberger.com>
2026-03-12 05:08:36 +0100

    update syllabe

--- static/games/syllabe/index.e50e2.js
+++ static/games/syllabe/index.e50e2.js
@@ -1,4 +1,4 @@
-System.register(["./application.fc72f.js"], function (_export, _context) {
+System.register(["./application.36e05.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
