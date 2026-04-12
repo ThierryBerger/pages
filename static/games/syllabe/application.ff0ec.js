7a1bf51bf61c58fdc70b07fd0af9c3bfbd0f756f
Thierry Berger <contact@thierryberger.com>
2026-04-12 22:05:13 +0200

    syllabe: lvl 4

--- static/games/syllabe/application.ff0ec.js
+++ static/games/syllabe/application.ff0ec.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.19baf.json';
+          this.settingsPath = 'src/settings.67eaf.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- static/games/syllabe/index.66321.js
+++ static/games/syllabe/index.66321.js
@@ -1,4 +1,4 @@
-System.register(["./application.ce57a.js"], function (_export, _context) {
+System.register(["./application.ff0ec.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
